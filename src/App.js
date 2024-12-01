eb29efc66e2d04c490127650bcfd140eb8ef09ae
meghs-nads <meghananadella871@gmail.com>
2024-12-01 18:39:17 +0530

    change 2 list megh

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ const App = () => {
     <div>
       <Navbar />
       
-      {/* <Hero /> */}
+      <ul>
+        <li>Apple</li>
+        <li>Ball</li>
+        <li>Cat</li>
+        <li>Dog</li>
+      </ul>
       <Footer />
     </div>
   );
