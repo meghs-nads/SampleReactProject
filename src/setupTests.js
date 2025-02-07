c7f96b37c979468ecf1b14b4b62b039ff5a58555
BOGUDA JAYAVARDHAN <152011279+jayavardhan-10@users.noreply.github.com>
2025-02-07 21:31:59 +0530

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,4 +4,20 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
-
+// Custom matcher example
+expect.extend({
+  toBeInTheDocument(received) {
+    const pass = received !== null && received !== undefined;
+    if (pass) {
+      return {
+        message: () => `expected ${received} not to be in the document`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () => `expected ${received} to be in the document`,
+        pass: false,
+      };
+    }
+  },
+});
