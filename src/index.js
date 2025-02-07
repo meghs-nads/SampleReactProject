197b7afbc591dd84a01f6e7f8d0b92bbfd51ca77
BOGUDA JAYAVARDHAN <152011279+jayavardhan-10@users.noreply.github.com>
2025-02-07 21:31:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,16 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("Error caught in ErrorBoundary: ", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; 
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
-
-
-
-
-
 );
 
 // If you want to start measuring performance in your app, pass a function
