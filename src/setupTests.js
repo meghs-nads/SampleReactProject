725d1079d701f9b46ff4eeed1491acb6b469ffd5
BOGUDA JAYAVARDHAN <152011279+jayavardhan-10@users.noreply.github.com>
2025-02-08 17:47:31 +0530

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -20,4 +20,20 @@ expect.extend({
       };
     }
   },
+
+  // New custom matcher to check if an element has a specific class
+  toHaveClass(received, className) {
+    const pass = received.classList.contains(className);
+    if (pass) {
+      return {
+        message: () => `expected ${received} not to have class ${className}`,
+        pass: true,
+      };
+    } else {
+      return {
+        message: () => `expected ${received} to have class ${className}`,
+        pass: false,
+      };
+    }
+  },
 });
